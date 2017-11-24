692a7d61c37e70306f0dd1a5185fec33d976d57d
okkhoury <okk6nb@virginia.edu>
2017-11-24 04:57:59 -0500

    added script to automate the running and opening of the visuals.

---
+++ SecondVisual/CreateVisual.sh
@@ -0,0 +1,31 @@
+echo ""
+echo ""
+echo "###################################################################################"
+echo ""
+echo "SECOND VISUALIZATION CREATION"
+echo ""
+echo "To update the topic words for model, copy the following map into affinityPlot.html: "
+echo ""
+Python topicWordsToJSMap.py
+echo ""
+echo ""
+echo "###################################################################################"
+echo ""
+echo ""
+read -n1 -p "Open affinityPlot.html? [y,n]" doit 
+case $doit in  
+  y|Y) vi affinityPlot.html;; 
+  n|N) echo ;; 
+  *) echo ;; 
+esac
+echo ""
+echo ""
+Python Affinity.py
+read -n1 -p "Open the visual? [y,n]" doit 
+case $doit in  
+  y|Y) open affinityPlot.html -a firefox;; 
+  n|N) echo ;; 
+  *) echo ;; 
+esac
+echo ""
+echo ""
---
+++ ThirdVisual/Yelp/CreateVisual.sh
@@ -0,0 +1,20 @@
+echo ""
+echo ""
+echo "###################################################################################"
+echo ""
+echo ""
+echo "THIRD VISUALIZATION CREATION"
+echo ""
+Python convertDataToTXT.py
+echo ""
+echo "###################################################################################"
+echo ""
+echo ""
+read -n1 -p "Open the visual? [y,n]" doit 
+case $doit in  
+  y|Y) open individualClusterPlot.html -a firefox;; 
+  n|N) echo ;; 
+  *) echo ;; 
+esac
+echo ""
+echo ""
