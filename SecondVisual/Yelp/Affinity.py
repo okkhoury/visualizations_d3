115f47d85ba9dba7d594e31a5ae6bb412d513d0a
okkhoury <okk6nb@virginia.edu>
2018-01-28 17:29:26 -0500

    updated yelp visual to only show top k connections

--- SecondVisual/Yelp/Affinity.py
+++ SecondVisual/Yelp/Affinity.py
@@ -1,4 +1,8 @@
 import numpy as np 
+import operator
+
+# Only shows the affinity for the top K clusters
+topK = 3
 
 # Determines how many edges get removed.
 EDGE_THRESHOLD = 0.1
@@ -13,32 +17,47 @@ curCol = 1
 jsonFile = open("YelpAffinity.json", "w+")
 jsonFile.write('{\n"links": [\n')
 
+# The number of lines to be written in the json file.
 numLines = 0
+
+topKMap = {}
 
 for row in range(0, rows):
 	for col in range(curCol, cols):
+		topKMap[col] = matrix[row][col]
 
-		if matrix[row][col] > EDGE_THRESHOLD:
-			numLines += 2
+	sorted_topK = sorted(topKMap.items(), key=operator.itemgetter(1), reverse = True)
 
+	for item in sorted_topK[:topK]:
+		topKCol = int(item[0])
+
+		if matrix[row][topKCol] != 0:
+			numLines += 1
+
+	topKMap.clear()
 	curCol += 1
 
 curCol = 1
 count = 0
+topKMap.clear()
 
 for row in range(0, rows):
 	for col in range(curCol, cols):
+		topKMap[col] = matrix[row][col]
 
-		if matrix[row][col] > EDGE_THRESHOLD:
-			count += 2
+	sorted_topK = sorted(topKMap.items(), key=operator.itemgetter(1), reverse = True)
+
+	for item in sorted_topK[:topK]:
+		topKCol = int(item[0])
+
+		if matrix[row][topKCol] != 0:
+			count += 1
 			if count == numLines:
-				jsonFile.write('{"source":' + '"' + str(row) + '","target":' + '"' + str(col) + '","affinity":' + str(matrix[row][col]) + '},\n')
-				jsonFile.write('{"source":' + '"' + str(col) + '","target":' + '"' + str(row) + '","affinity":' + str(matrix[row][col]) + '}\n')
+				jsonFile.write('{"source":' + '"' + str(row) + '","target":' + '"' + str(topKCol) + '","affinity":' + str(matrix[row][topKCol]) + '}\n')
 			else:
-				jsonFile.write('{"source":' + '"' + str(row) + '","target":' + '"' + str(col) + '","affinity":' + str(matrix[row][col]) + '},\n')
-				jsonFile.write('{"source":' + '"' + str(col) + '","target":' + '"' + str(row) + '","affinity":' + str(matrix[row][col]) + '},\n')
+				jsonFile.write('{"source":' + '"' + str(row) + '","target":' + '"' + str(topKCol) + '","affinity":' + str(matrix[row][topKCol]) + '},\n')
 
-			
+	topKMap.clear()	
 	curCol += 1
 
 jsonFile.write(']\n}')
